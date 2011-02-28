839c779dd86ac5ecaa11dbd371dd48c30a3bb489
Igor P. Leroy <ip.leroy@gmail.com>
2011-02-28 12:50:19 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'pg'
+gem 'jquery-rails'
+
+# HAML
+gem 'haml'
+gem 'hpricot'
+gem 'ruby_parser'
+
+# Authentication
+gem 'devise', "1.1.7"
+gem 'cancan', "1.5.1"
+
+group :test, :development do
+  gem "cucumber-rails", "0.3.2"
+  gem "rspec-rails", "2.5.0"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniShop::Application.config.secret_token = '0b2f372b420cde9b229b23cc5b2148b5b7d77b4c0229a8e2271747a1a54876303135279ac0fb46598cbcfd53f592b625a04c2fcf3abaa2419f6b45a9f1493b31'
